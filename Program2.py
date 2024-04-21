e7183cfcd5673d354cad2e834deaab7577f4c7a9
janaali17 <ja2200300@tkh.edu.eg>
2024-04-21 10:44:01 +0200

    Update Program2.py

---
+++ Program2.py
@@ -0,0 +1,9 @@
+import math # so that python can understand any mathmatical equation
+import time # figures out time taken to excute the code
+import mainprogram # extracting functions from a different file
+
+def facrtorization(n,e):
+    start_time= time.perf_counter()
+    factorize.append(2)
+    n= n//2
+    for i in range(3,int())
